ef4309a4f644bcb6fb0a712e28a4df1486d1926d
Allef Gomes <allefgalmeida@gmail.com>
2025-04-01 23:03:30 -0300

    feat: cria funcionalidade de adicionar o resumo

---
+++ estudos_biblicos_basico/js/add_resumo.js
@@ -0,0 +1,22 @@
+document
+  .getElementById('resumoForm')
+  .addEventListener('submit', function(event) {
+    event.preventDefault();
+    const referencia = document.getElementById('referencia').value;
+    const resumo = document.getElementById('resumo').value;
+
+    const listItem = document.createElement('li');
+    const title = document.createElement('h3');
+    title.textContent = referencia;
+    const description = document.createElement('p');
+    description.textContent = resumo;
+    
+    listItem.appendChild(title);
+    listItem.appendChild(description);
+    
+    document.getElementById('resumos').appendChild(listItem);
+
+    document.getElementById('referencia').value = '';
+    document.getElementById('resumo').value = '';
+  }
+);
